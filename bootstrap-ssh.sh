670039ccdeddaf602064328edde758f16ccbf090
barnxba <barnxba@gmail.com>
2022-07-09 14:03:24 +0200

    Reorganize using functions

--- bootstrap-ssh.sh
+++ bootstrap-ssh.sh
@@ -11,27 +11,45 @@ usage() {
 }
 if [ -z "$1" ]; then (usage; exit 1); fi
 
-image="$1"
-loop_device=$(sudo losetup --show -f "$image")
-sudo partx -a $loop_device
-root=$(lsblk $loop_device -o NAME -lnp | head -n3 | tail -n1)
-mount_dir=$(mktemp -d -t rpi-root-XXXXXX)
-echo "Mounting Rasberry OS image in: $mount_dir"
-sudo mount $root $mount_dir
-echo "Setting new password for user pi"
-pass=$(openssl passwd -1)
-sudo sed -i -e "s@\(^pi:\)\([^:]*\)\(.*$\)@\1${pass}\3@" $mount_dir/etc/shadow
-echo "Removing nopasswd access from user pi"
-sudo rm -f $mount_dir/etc/sudoers.d/010_pi-nopasswd
-echo "Enabling SSH"
-sudo ln -sf $mount_dir/lib/systemd/system/ssh.service $mount_dir/etc/systemd/system/sshd.service
-sudo ln -sf $mount_dir/lib/systemd/system/ssh.service $mount_dir/etc/systemd/system/multi-user.target.wants/ssh.service
-for d in rc2.d rc3.d rc4.d rc5.d; do (cd $mount_dir/etc/$d; sudo ln -sf ../init.d/ssh S01ssh); done
-find $mount_dir/etc/rc* -name 'K*ssh' -exec sudo rm {} +
+function mount_image {
+    loop_device=$(sudo losetup --show -f "$image")
+    sudo partx -a $loop_device
+    root=$(lsblk $loop_device -o NAME -lnp | head -n3 | tail -n1)
+    mount_dir=$(mktemp -d -t rpi-root-XXXXXX)
+    echo "Mounting Raspberry OS image in: $mount_dir"
+    sudo mount $root $mount_dir
+}
 
-# cleanup
-sudo umount $root
-sudo partx -d $loop_device
-sudo losetup -d $loop_device
-rmdir $mount_dir
-echo "Done"
+function update_passwd {
+    echo "Setting new password for user pi"
+    pass=$(openssl passwd -1)
+    sudo sed -i -e "s@\(^pi:\)\([^:]*\)\(.*$\)@\1${pass}\3@" $mount_dir/etc/shadow
+}
+
+function remove_nopasswd_sudo {
+    echo "Removing nopasswd access from user pi"
+    sudo rm -f $mount_dir/etc/sudoers.d/010_pi-nopasswd
+}
+
+function enable_ssh {
+    echo "Enabling SSH"
+    sudo ln -sf $mount_dir/lib/systemd/system/ssh.service $mount_dir/etc/systemd/system/sshd.service
+    sudo ln -sf $mount_dir/lib/systemd/system/ssh.service $mount_dir/etc/systemd/system/multi-user.target.wants/ssh.service
+    for d in rc2.d rc3.d rc4.d rc5.d; do (cd $mount_dir/etc/$d; sudo ln -sf ../init.d/ssh S01ssh); done
+    find $mount_dir/etc/rc* -name 'K*ssh' -exec sudo rm {} +
+}
+
+function cleanup {
+    sudo umount $root
+    sudo partx -d $loop_device
+    sudo losetup -d $loop_device
+    rmdir $mount_dir
+    echo "Done"
+}
+
+image="$1"
+mount_image
+update_passwd
+remove_nopasswd_sudo
+enable_ssh
+cleanup
